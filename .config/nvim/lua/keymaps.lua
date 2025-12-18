ba08818dc409a7617e38d2a515e681612b2b3459
jet-inigo <jetgeronimo@gmail.com>
2025-12-17 23:53:39 -0600

    Add hotkey to toggle diagnostics

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -3,3 +3,12 @@ local opts = { noremap = true, silent = true }
 -- Create splits
 vim.keymap.set("n", "<C-w>u", ":split<CR>", opts)
 vim.keymap.set("n", "<C-w>i", ":vsplit<CR>", opts)
+
+-- Toggle diagnostics
+vim.keymap.set("n", "<leader>h", function()
+  if vim.diagnostic.is_enabled() then
+    vim.diagnostic.enable(false)
+  else
+    vim.diagnostic.enable(true)
+  end
+end, { desc = "Toggle diagnostics" })
