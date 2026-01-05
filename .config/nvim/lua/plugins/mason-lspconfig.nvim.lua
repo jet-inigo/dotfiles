a79a3a3cf3e9ff4432a509da8be454ae4f2d7ed7
jet-inigo <jetgeronimo@gmail.com>
2026-01-05 02:16:34 -0600

    Edit mason-lspconfig.nvim.lua to ensure certain LSPs and provide a default handler

--- .config/nvim/lua/plugins/mason-lspconfig.nvim.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.nvim.lua
@@ -1,5 +1,24 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
-    opts = {},
+  "mason-org/mason-lspconfig.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mason.nvim",
+  },
+  opts = {
+    ensure_installed = {
+      "basedpyright",
+      "clangd",
+      "csharp_ls",
+    },
+
+    handlers = {
+      -- Default handler for all servers
+      function(server_name)
+        require("lspconfig")[server_name].setup({
+          capabilities = capabilities,
+          settings = {},
+        })
+      end,
+    },
+  },
 }
