019bc73b81269a0d23ed74feb95ec787c9f95cc8
jet-inigo <jetgeronimo@gmail.com>
2026-01-05 02:17:04 -0600

    Remove tmux from config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -36,14 +36,3 @@ funcsave -q vim
 zoxide init --cmd cd fish | source
 starship init fish | source
 pyenv init - fish | source
-
-function start_tmux
-    if type tmux > /dev/null
-        #if not inside a tmux session, and if no session is started, start a new session
-        if test -z "$TMUX" ; and test -z $TERMINAL_CONTEXT
-            tmux -2 attach; or tmux -2 new-session
-        end
-    end
-end
-
-start_tmux
