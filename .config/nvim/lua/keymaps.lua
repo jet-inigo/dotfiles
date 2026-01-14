58f3b0714b0f369101710c96960759e4341a0628
jet-inigo <jetgeronimo@gmail.com>
2026-01-14 01:37:31 -0600

    Add - hotkey for mini.files.nvim

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -12,3 +12,11 @@ vim.keymap.set("n", "<leader>h", function()
     vim.diagnostic.enable(true)
   end
 end, { desc = "Toggle diagnostics" })
+
+-- Open mini.files
+vim.keymap.set("n", "-", function()
+  local buf_name = vim.api.nvim_buf_get_name(0)
+  local path = vim.fn.filereadable(buf_name) == 1 and buf_name or vim.fn.getcwd()
+  MiniFiles.open(path)
+  MiniFiles.reveal_cwd()
+end, { desc = "Open Mini Files" })
