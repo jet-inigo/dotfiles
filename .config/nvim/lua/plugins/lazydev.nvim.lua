2744167565e2a74024448299f45a1aabd0e6d328
jet-inigo <jetgeronimo@gmail.com>
2026-01-17 01:16:01 -0600

    Add vim as lua global in lazydev.nvim

--- .config/nvim/lua/plugins/lazydev.nvim.lua
+++ .config/nvim/lua/plugins/lazydev.nvim.lua
@@ -1,4 +1,9 @@
 return {
     "https://github.com/folke/lazydev.nvim",
     ft = "lua",
+    opts = {
+        library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        },
+    }
 }
