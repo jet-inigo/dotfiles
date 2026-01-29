29aefa1f7844a03a84a9f05e162e8388870e9a6a
jet-inigo <jetgeronimo@gmail.com>
2026-01-29 15:23:52 -0600

    Add roslyn.nvim

---
+++ .config/nvim/lua/plugins/roslyn.nvim.lua
@@ -0,0 +1,10 @@
+return {
+    "seblyng/roslyn.nvim",
+    ft = { "cs", "razor", "cshtml" },
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+        -- your configuration comes here; leave empty for default settings
+    },
+}
+
