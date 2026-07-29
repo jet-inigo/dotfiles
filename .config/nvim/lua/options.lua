b0f5df0aa2513dadd44589ddfd3c615239f79657
jet-inigo <jetgeronimo@gmail.com>
2026-07-29 00:10:44 -0500

    Replace deprecated call

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -37,7 +37,7 @@ vim.diagnostic.config({
 })
 
 -- Yank to system clipboard
-vim.api.nvim_set_option("clipboard", "unnamedplus")
+vim.api.nvim_set_option_value("clipboard", "unnamedplus", {})
 
 -- Disable auto comment
 vim.cmd([[autocmd FileType * set formatoptions-=cro]])
